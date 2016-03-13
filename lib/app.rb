74b02a1fcbd54e754d1a041db3afb41f31c6ba06
Sean <sjgallen@gmail.com>
2016-03-13 16:55:59 -0500

    removed unneeded comments from app.rb

--- lib/app.rb
+++ lib/app.rb
@@ -1,26 +1,5 @@
-# require 'rack'
 require 'socket'
-require 'pry'
 
-#Below is the ruby code for setting up a simple rack server
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#
-#================= Steps that Still Need to be Completed =============================
-#1. [ done ] define server as server = TCPServer ( :host, :port)
-#2. [ done ] TCPServer.accept = socket
-#3. [  ]socket.parse  (where parse is our method for parsing the steam, has to be unit tested)
-#4. [  ]****** parse will return a hash *******
-#5. [  ]****** hash is handed to @app *******
-#6. [  ]***** app returns an array of [status, headers, body] *******
-#7. [  ]***** socket.print/puts array *********
-#8. [  ]***** close the socket **********
-#9. [  ]***** next iteration: for multiple request handling, set up a loop *****
-#10.[  ]***** ATSOME POINT figure out where/when in code to close server ***********
-#
-#
-#
-#***for unit test might need rack.input as a commmand of some sort ****
-#*** unit test also needs to use stringerIO to feed string *********c
 
 class Notes
   class Web
@@ -38,7 +17,6 @@ class Notes
       loop do
         socket = @server.accept
 
-        # binding.pry
         method, path, http_version = socket.gets.chomp.split(" ")
 
         puts "#{method}, #{path}, #{http_version}"
